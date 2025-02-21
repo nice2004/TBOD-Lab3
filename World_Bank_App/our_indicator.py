ef4c7c88e149b789a2fadfa0491c50eecc0dc0d0
nice <nhirwa@westmont.edu>
2025-02-20 21:02:08 -0800

    Halfway done with task 2

--- World_Bank_App/our_indicator.py
+++ World_Bank_App/our_indicator.py
@@ -2,5 +2,6 @@ import pandas as pd
 from pandas_datareader import wb
 
 df = wb.get_indicators()[['id', 'name']]
-df = df[df.name == 'Individuals using the Internet (% of population)']
+df = df[df.name == 'Mortality rate, under-5 (per 1,000 live births)']
 print(df)
+
--- World_Bank_App/worldbank.py
+++ World_Bank_App/worldbank.py
@@ -4,12 +4,12 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 from pandas_datareader import wb
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
 indicators = {
     "IT.NET.USER.ZS": "Individuals using the Internet (% of population)",
     "SG.GEN.PARL.ZS": "Proportion of seats held by women in national parliaments (%)",
-    "EN.ATM.CO2E.KT": "CO2 emissions (kt)",
+    "SH.DYN.MORT": "Mortality rate, under-5 (per 1,000 live births)",
 }
 
 # get country name and ISO id for mapping on choropleth
@@ -18,6 +18,7 @@ countries["capitalCity"].replace({"": None}, inplace=True)
 countries.dropna(subset=["capitalCity"], inplace=True)
 countries = countries[["name", "iso3c"]]
 countries = countries[countries["name"] != "Kosovo"]
+countries = countries[countries["name"] != "Korea, Dem. People's Rep."]
 countries = countries.rename(columns={"name": "country"})
 
 
@@ -32,7 +33,7 @@ def update_wb_data():
     # Add country ISO3 id to main df
     df = pd.merge(df, countries, on="country")
     df = df.rename(columns=indicators)
-    print(df)
+
     return df
 
 
@@ -45,12 +46,16 @@ app.layout = dbc.Container(
                         "Comparison of World Bank Country Data",
                         style={"textAlign": "center"},
                     ),
+                    html.H2(
+                        "Last fetched out in 2016 and [time]",
+                        style={'textAlign': 'center'}
+                    ),
                     dcc.Graph(id="my-choropleth", figure={}),
                 ],
                 width=12,
             )
         ),
-        dbc.Row(
+        dbc.Row([
             dbc.Col(
                 [
                     dbc.Label(
@@ -58,57 +63,55 @@ app.layout = dbc.Container(
                         className="fw-bold",
                         style={"textDecoration": "underline", "fontSize": 20},
                     ),
-                    dcc.RadioItems(
-                        id="radio-indicator",
+                    dcc.Dropdown(
+                        id="i-dropdown",
                         options=[{"label": i, "value": i} for i in indicators.values()],
                         value=list(indicators.values())[0],
-                        inputClassName="me-2",
+                        className="three columns",
                     ),
                 ],
                 width=4,
-            )
-        ),
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        dbc.Label(
-                            "Select Years:",
-                            className="fw-bold",
-                            style={"textDecoration": "underline", "fontSize": 20},
-                        ),
-                        dcc.RangeSlider(
-                            id="years-range",
-                            min=2005,
-                            max=2016,
-                            step=1,
-                            value=[2005, 2006],
-                            marks={
-                                2005: "2005",
-                                2006: "'06",
-                                2007: "'07",
-                                2008: "'08",
-                                2009: "'09",
-                                2010: "'10",
-                                2011: "'11",
-                                2012: "'12",
-                                2013: "'13",
-                                2014: "'14",
-                                2015: "'15",
-                                2016: "2016",
-                            },
-                        ),
-                        dbc.Button(
-                            id="my-button",
-                            children="Submit",
-                            n_clicks=0,
-                            color="primary",
-                            className="mt-4 fw-bold",
-                        ),
-                    ],
-                    width=6,
-                ),
-            ]
+            ),
+
+            dbc.Col(
+                [
+                    dbc.Label(
+                        "Select Years:",
+                        className="fw-bold",
+                        style={"textDecoration": "underline", "fontSize": 20},
+                    ),
+                    dcc.RangeSlider(
+                        id="years-range",
+                        min=2005,
+                        max=2016,
+                        step=1,
+                        value=[2005, 2006],
+                        marks={
+                            2005: "2005",
+                            2006: "'06",
+                            2007: "'07",
+                            2008: "'08",
+                            2009: "'09",
+                            2010: "'10",
+                            2011: "'11",
+                            2012: "'12",
+                            2013: "'13",
+                            2014: "'14",
+                            2015: "'15",
+                            2016: "2016",
+                        },
+                    ),
+                    dbc.Button(
+                        id="my-button",
+                        children="Submit",
+                        n_clicks=0,
+                        color="primary",
+                        className="d-flex justify-content-end bg-light border fw-bold",
+                    ),
+                ],
+                width=6,
+            ),
+        ]
         ),
         dcc.Store(id="storage", storage_type="session", data={}),
         dcc.Interval(id="timer", interval=1000 * 10, n_intervals=0),
@@ -127,7 +130,7 @@ def store_data(n_time):
     Input("my-button", "n_clicks"),
     Input("storage", "data"),
     State("years-range", "value"),
-    State("radio-indicator", "value"),
+    State("i-dropdown", "value"),
 )
 def update_graph(n_clicks, stored_dataframe, years_chosen, indct_chosen):
     dff = pd.DataFrame.from_records(stored_dataframe)
@@ -147,6 +150,7 @@ def update_graph(n_clicks, stored_dataframe, years_chosen, indct_chosen):
             labels={
                 indicators["SG.GEN.PARL.ZS"]: "% parliament women",
                 indicators["IT.NET.USER.ZS"]: "pop % using internet",
+                indicators['SH.DYN.MORT']: 'Mortality rate, under-5 (per 1,000 live births)',
             },
         )
         fig.update_layout(
@@ -166,6 +170,7 @@ def update_graph(n_clicks, stored_dataframe, years_chosen, indct_chosen):
             labels={
                 indicators["SG.GEN.PARL.ZS"]: "% parliament women",
                 indicators["IT.NET.USER.ZS"]: "pop % using internet",
+                indicators['SH.DYN.MORT']: 'Mortality rate, under-5 (per 1,000 live births)',
             },
         )
         fig.update_layout(
